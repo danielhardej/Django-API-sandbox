ab2cf5932887d69d7e4f9fd880835d3b1702428e
Dan Hardej <danielhardej@github.com>
2025-02-05 19:49:55 +0800

    Update settings.py

--- APIsandbox/settings.py
+++ APIsandbox/settings.py
@@ -119,10 +119,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/stable/howto/static-files/
 
-# STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 
 # Define the directory where collectstatic will collect static files for deployment
-STATIC_ROOT = os.path.join(BASE_DIR, '/staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Additional static files settings (optional)
 # STATICFILES_DIRS = [
