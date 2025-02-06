2b2827d451173d71e266675a798a6c8fff843ae3
Dan Hardej <danielhardej@github.com>
2025-02-06 15:20:30 +0800

    Update database settings

--- APIsandbox/settings.py
+++ APIsandbox/settings.py
@@ -78,11 +78,14 @@ WSGI_APPLICATION = 'APIsandbox.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ["PGDATABASE"],
+        'USER': os.environ["PGUSER"],
+        'PASSWORD': os.environ["PGPASSWORD"],
+        'HOST': os.environ["PGHOST"],
+        'PORT': os.environ["PGPORT"],
     }
 }
 
