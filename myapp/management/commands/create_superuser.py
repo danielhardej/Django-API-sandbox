891d096dddfe11dd1f28c63424ecec546693ffb2
Dan Hardej <danielhardej@github.com>
2025-02-06 16:34:49 +0800

    Remove create superuser step

--- myapp/management/commands/create_superuser.py
+++ myapp/management/commands/create_superuser.py
@@ -1,6 +1,6 @@
 import os
 from django.core.management.base import BaseCommand
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 
 class Command(BaseCommand):
     help = 'Create a superuser non-interactively at deployment'
@@ -11,6 +11,7 @@ class Command(BaseCommand):
         password = os.getenv('DJANGO_SUPERUSER_PASSWORD')
 
         try:
+            User = get_user_model()
             if not User.objects.filter(username=username).exists():
                 User.objects.create_superuser(username=username, email=email, password=password)
                 self.stdout.write(self.style.SUCCESS(f'Superuser "{username}" created successfully'))
